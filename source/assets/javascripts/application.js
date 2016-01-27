ff652bb93e8b8e77fc32c01d6ba33f00d8b9c714
Eric Gardner <gardner.ec@gmail.com>
2016-01-26 20:39:50 -0800

    Wire up JS for responsive nav and sticky sidebar

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,37 @@
 //= require_tree .
+
+// Utility functions
+function debounce(fn, delay) {
+  var timer = null;
+  return function () {
+    var context = this, args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  };
+}
+
+// Main method
+$(document).ready(function () {
+  // nav menu
+  $(".nav-menu-icon a").click(function () {
+    $(".navigation-menu").toggleClass("active");
+  });
+
+  // Sticky sidebar
+  var $sidebar = $(".page-content-sidebar");
+  var offset = $sidebar.offset().top - 20 || undefined;
+  var currentScroll = 0;
+
+  window.addEventListener('scroll', function () {
+    currentScroll = $(window).scrollTop();
+    console.log(currentScroll);
+    if (currentScroll > offset) {
+      $sidebar.find('ul').addClass("sticky");
+    } else if (currentScroll <= offset ) {
+      $sidebar.find('ul').removeClass("sticky");
+    }
+  })
+
+});
